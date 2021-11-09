e1719005e42be49430dca8f4141c130d41ab13d1
Leonard Choo <leochoodev@gmail.com>
2021-11-09 16:53:43 +0900

    rxjs and rxfire authenticatio done

--- src/lib/Login.svelte
+++ src/lib/Login.svelte
@@ -1,12 +1,23 @@
 <script>
   import { auth } from "../../firebase.js";
   import { signInAnonymously } from "firebase/auth";
+  import { authState } from "rxfire/auth";
+  let user;
+  const unsubscribe = authState(auth).subscribe((u) => (user = u));
+  function login() {
+    signInAnonymously(auth);
+  }
 </script>
 
 <main>
-  <button
-    type="button"
-    class="btn btn-dark"
-    on:click={() => signInAnonymously(auth)}>Sign In Anonymously</button
-  >
+  {#if user}
+    <button type="button" class="btn btn-dark" on:click={() => auth.signOut()}
+      >Logout</button
+    >
+    <hr />
+  {:else}
+    <button type="button" class="btn btn-dark" on:click={login}>
+      Sign In Anonymously
+    </button>
+  {/if}
 </main>
