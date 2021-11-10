c4364550331a4954fc9111819af70c72a4578920
Leonard Choo <leochoodev@gmail.com>
2021-11-10 14:11:18 +0900

    record voice shown

--- src/lib/Upload.svelte
+++ src/lib/Upload.svelte
@@ -1,3 +1,58 @@
+<script>
+  let loading = false;
+  let newAudio = null;
+  let recorder = null;
+
+  async function record() {
+    newAudio = null;
+
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: false,
+    });
+
+    const options = { mimeType: "audio/webm" };
+    const recordedChunks = [];
+    recorder = new MediaRecorder(stream, options);
+
+    recorder.addEventListener("dataavailable", (e) => {
+      if (e.data.size > 0) {
+        recordedChunks.push(e.data);
+      }
+    });
+
+    recorder.addEventListener("stop", () => {
+      newAudio = new Blob(recordedChunks);
+      console.log(newAudio);
+    });
+
+    recorder.start();
+  }
+  async function stop() {
+    recorder.stop();
+    recorder = null;
+  }
+</script>
+
 <main>
   <h1>Upload</h1>
+  <h5>Record Audio</h5>
+
+  {#if !recorder}
+    <button on:click={() => record()} class="button is-info"
+      >Record Voice</button
+    >
+  {:else}
+    <button on:click={() => stop()} class="button is-danger">Stop</button>
+  {/if}
+
+  <br />
+
+  {#if newAudio}
+    <audio controls src={URL.createObjectURL(newAudio)} />
+  {/if}
+
+  <hr />
+
+  <button type="text">Send</button>
 </main>
