2fe0678cfdb01ce8c28d7032d34ce91a8a3070c8
Leonard Choo <leochoodev@gmail.com>
2021-11-10 14:37:55 +0900

    file upload complete

--- src/lib/Upload.svelte
+++ src/lib/Upload.svelte
@@ -1,4 +1,7 @@
 <script>
+  import { storage } from "../../firebase.js";
+  import { ref, uploadBytes } from "firebase/storage";
+
   let loading = false;
   let newAudio = null;
   let recorder = null;
@@ -32,6 +35,51 @@
     recorder.stop();
     recorder = null;
   }
+
+  // upload audio to Firebase storage and get download url
+  async function upload() {
+    if (newAudio) {
+      const storageRef = ref(storage, "audio");
+
+      // 'file' comes from the Blob or File API
+      uploadBytes(storageRef, newAudio).then((snapshot) => {
+        console.log("Uploaded a blob or file!");
+      });
+
+      // const uploadTask = storageRef.child(new Date().getTime()).put(newAudio);
+      // uploadTask.on(
+      //   "state_changed",
+      //   (snapshot) => {
+      //     // Observe state change events such as progress, pause, and resume
+      //     // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+      //     const progress =
+      //       (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      //     console.log("Upload is " + progress + "% done");
+      //     switch (snapshot.state) {
+      //       case "paused": // or 'paused'
+      //         console.log("Upload is paused");
+      //         break;
+      //       case "running": // or 'running'
+      //         console.log("Upload is running");
+      //         break;
+      //     }
+      //   },
+      //   (error) => {
+      //     // Handle unsuccessful uploads
+      //     console.log(error);
+      //   },
+      //   () => {
+      //     // Handle successful uploads on complete
+      //     // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+      //     uploadTask.snapshot.ref.getDownloadURL().then((downloadURL) => {
+      //       console.log("File available at", downloadURL);
+      //       document.getElementById("audio-url").value = downloadURL;
+      //     });
+      //   }
+      //   );
+      // }
+    }
+  }
 </script>
 
 <main>
@@ -54,5 +102,5 @@
 
   <hr />
 
-  <button type="text">Send</button>
+  <button type="text" on:click={() => upload()}>Send</button>
 </main>
